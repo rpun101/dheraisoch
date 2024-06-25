7094e80b07880ad59c754483a08dcc315fbb8665
Rob <rpun101@users.noreply.github.com>
2024-06-25 00:13:14 -0700

    dummy-first-page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Rabin writing Corner',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
